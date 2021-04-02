6e913e534ac8acbfd0e94c24768d27ef56778b6f
ljackson140 <llakujack@outlook.com>
2021-04-02 16:21:27 +1000

    animation distortion for images

--- animation.js
+++ animation.js
@@ -1,3 +1,17 @@
+//Start of animation
+
+/**
+ * Distortion effect of Sneakers
+ */
 new hoverEffect({
-    
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './img/White -AF1.png',
+    image2: './img/Black-AF1.png',
+    displacementImage: './img/diss.png',
+    imagesRatio: 380 / 300
 })
+
+/**
+ * NAVBAR
+ */
